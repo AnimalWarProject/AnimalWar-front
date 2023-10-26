0eb965e6dc438a6ef9892b1c8e7dc951a59777dc
JUNGI JEONG <junejjk@gmail.com>
2023-10-26 15:59:42 +0900

    'merged'

--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Rank from '../pages/Ranking/components/Rank';
+import Background from '../common/components/Background';
 
 const MyRoutes = () => {
     return (
         <Routes>
             <Route path="/rank" element={<Rank />}></Route>
+            <Route path="/test" element={<Background />}></Route>
             {/* <Route path="/redirect" element={<KakaoLoginRedirect />}></Route>
               <Route path="/logout" element={<Logout />}></Route> */}
         </Routes>
