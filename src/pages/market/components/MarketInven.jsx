6b6d5a89b46a56057a958464a48383e81d90b4a5
Jeongsu6637 <wjdtn8738@gmail.com>
2023-10-27 17:18:58 +0900

    #1 Market 와이어 프레임

--- src/pages/market/components/MarketInven.jsx
+++ src/pages/market/components/MarketInven.jsx
@@ -5,7 +5,39 @@ const MarketInven = () => {
 
 
     return <>
-        <div className="register-container">
+        <div className="marketinven-container">
+            <div className="marketinven-section">
+                <div className="marketinven-wrap">
+                    <div className="marketinven-content">1</div>
+                    <div className="marketinven-content">1</div>
+                    <div className="marketinven-content">1</div>
+                </div>
+                <div className="marketinven-wrap">
+                    <div className="marketinven-content">1</div>
+                    <div className="marketinven-content">1</div>
+                    <div className="marketinven-content">1</div>
+                </div>
+                <div className="marketinven-wrap">
+                    <div className="marketinven-content">1</div>
+                    <div className="marketinven-content">1</div>
+                    <div className="marketinven-content">1</div>
+                </div>
+                <div className="marketinven-wrap">
+                    <div className="marketinven-content">1</div>
+                    <div className="marketinven-content">1</div>
+                    <div className="marketinven-content">1</div>
+                </div>
+                <div className="marketinven-wrap">
+                    <div className="marketinven-content">1</div>
+                    <div className="marketinven-content">1</div>
+                    <div className="marketinven-content">1</div>
+                </div>
+                <div className="marketinven-wrap">
+                    <div className="marketinven-content">1</div>
+                    <div className="marketinven-content">1</div>
+                    <div className="marketinven-content">1</div>
+                </div>
+            </div>
         </div>
     </>;
 };
