ec8a6c868d0446a969a1b811beee44a1f07ac335
sein <lsi6601@gmail.com>
2023-10-27 14:21:55 +0900

    init

--- src/template/GameTemplate.jsx
+++ src/template/GameTemplate.jsx
@@ -14,6 +14,7 @@ const GameTemplate = () => {
                 <div className="content-container">
                     <GameScreenTest />
                     <ResourceBox />
+
                 </div>
             </Background>
         </>
