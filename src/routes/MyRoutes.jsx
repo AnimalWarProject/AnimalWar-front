fe453351960fc86dcc9eca3f0f74122223cc4624
JUNGI JEONG <junejjk@gmail.com>
2023-10-27 17:47:39 +0900

    R r changed

--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -3,20 +3,18 @@ import BattlePage from '../pages/battle/BattlePage';
 import GameTemplate from '../template/GameTemplate';
 import BasicTemplate from '../template/BasicTemplate';
 import RankingPage from '../pages/ranking/RankingPage';
-import MarketPage from "../pages/market/components/MarketPage";
-
+import MarketPage from '../pages/market/components/MarketPage';
 
 const MyRoutes = () => {
     return (
         <Routes>
-
             <Route path="/rank" element={<RankingPage />}></Route>
 
             <Route element={<GameTemplate />}>
                 <Route path="/test" element={<BattlePage />}></Route>
             </Route>
             <Route element={<BasicTemplate />}>
-                 <Route path="/market" element={<MarketPage />}></Route>
+                <Route path="/market" element={<MarketPage />}></Route>
             </Route>
         </Routes>
     );
