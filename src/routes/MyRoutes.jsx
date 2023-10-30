4397643f953e1c5a6e88b9ed8617b70a673ea2a4
JUNGI JEONG <junejjk@gmail.com>
2023-10-30 17:33:11 +0900

    Login, Singup, myPage updating

--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -2,13 +2,17 @@ import { Routes, Route } from 'react-router-dom';
 import BattlePage from '../pages/battle/BattlePage';
 import GameTemplate from '../template/GameTemplate';
 import BasicTemplate from '../template/BasicTemplate';
-import RankingPage from '../pages/ranking/RankingPage';
+import RankingPage from '../pages/Ranking/RankingPage';
 import MarketPage from '../pages/market/components/MarketPage';
+import LoginPage from '../pages/login/LoginPage';
+import SignupPage from '../pages/signup/SignupPage';
 
 const MyRoutes = () => {
     return (
         <Routes>
             <Route path="/rank" element={<RankingPage />}></Route>
+            <Route path="/login" element={<LoginPage />}></Route>
+            <Route path="/signup" element={<SignupPage />}></Route>
 
             <Route element={<GameTemplate />}>
                 <Route path="/test" element={<BattlePage />}></Route>
