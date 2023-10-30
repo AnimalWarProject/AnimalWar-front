47b060d83875aab7c11cd8565a82c0b83a00ac96
unknown <you@example.com>
2023-10-30 23:02:30 +0900

    Chore: #15 chatting 화면 완성

--- src/common/components/Chatting.jsx
+++ src/common/components/Chatting.jsx
@@ -1,5 +1,9 @@
 import classes from "./Chatting.module.css"
+import Rectangle3 from "../imgs/Rectangle 3.png"
 import Rectangle4 from "../imgs/Rectangle 4.png"
+import Rectangle5 from "../imgs/Rectangle 5.png"
+import Rectangle6 from "../imgs/Rectangle 6.png"
+import Rectangle7 from "../imgs/Rectangle 7.png"
 import sendIcon from "../imgs/send.png"
 import plusIcon from "../imgs/plusIcon.png"
 import {useState} from "react";
@@ -7,6 +11,7 @@ import {useState} from "react";
 const Chatting = () => {
     const [inputText, setInputText] = useState('');
     const maxLength = 100; // 최대 글자 수
+    const randomColorClass = 'random-color'; // 랜덤 색상 클래스 이름
 
     const handleInputChange = (event) => {
         const text = event.target.value;
@@ -20,14 +25,52 @@ const Chatting = () => {
     };
 
 
+
+
+// 랜덤한 16진수 색상 코드를 생성하는 함수
+    const randomColor = getRandomColor(); // 랜덤한 색상 코드를 가져옵니다
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+
+
+
+
     return (
         <section className={classes.Wrap}>
             <div className={classes.box}>
                 <div className={classes.title}>CHATTING</div>
                 <div className={classes.message}>
+                    <div className={classes.message_title}>GOOD CHATTING PLZ!</div>
                     <div className={classes.message_container}>
                         <img className={classes.profileImg} src={Rectangle4}/>
-                        <div className={classes.profileNickname}>Cat:</div>
+                        <div className={classes.profileNickname} style={{ color: randomColor }}>Cat:</div>
+                        <div className={classes.messageRecord}>유례없는 흉작으로 사과 생산량이 감소해 가격이 치솟자</div>
+                    </div>
+                    <div className={classes.message_container}>
+                        <img className={classes.profileImg} src={Rectangle5}/>
+                        <div className={classes.profileNickname} style={{ color: randomColor }}>Cat:</div>
+                        <div className={classes.messageRecord}>유례없는 흉작으로 사과 생산량이 감소해 가격이 치솟자</div>
+                    </div>
+                    <div className={classes.message_container}>
+                        <img className={classes.profileImg} src={Rectangle3}/>
+                        <div className={classes.profileNickname} style={{ color: randomColor }}>Cat:</div>
+                        <div className={classes.messageRecord}>유례없는 흉작으로 사과 생산량이 감소해 가격이 치솟자</div>
+                    </div>
+                    <div className={classes.message_container}>
+                        <img className={classes.profileImg} src={Rectangle6}/>
+                        <div className={classes.profileNickname} style={{ color: randomColor }}>Cat:</div>
+                        <div className={classes.messageRecord}>유례없는 흉작으로 사과 생산량이 감소해 가격이 치솟자</div>
+                    </div>
+                    <div className={classes.message_container}>
+                        <img className={classes.profileImg} src={Rectangle7}/>
+                        <div className={classes.profileNickname} style={{ color: randomColor }}>Cat:</div>
                         <div className={classes.messageRecord}>유례없는 흉작으로 사과 생산량이 감소해 가격이 치솟자</div>
                     </div>
                 </div>
