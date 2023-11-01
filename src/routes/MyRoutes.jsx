220bfa84285a57dafbd084a2a23a745f3e8ef94c
JUNGI JEONG <junejjk@gmail.com>
2023-11-01 15:56:35 +0900

    Firebase revised

--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -8,24 +8,20 @@ import Match from '../pixi/Match/Match';
 import DrawPage from '../pixi/draw/components/DrawPage';
 import LoginPage from '../pages/login/LoginPage';
 import SignupPage from '../pages/signup/SignupPage';
-import Mix from "../pixi/Mix/Mix";
-import DrawResult from "../pixi/draw/components/DrawResult";
-import React from "react";
+import Mix from '../pixi/Mix/Mix';
+import DrawResult from '../pixi/draw/components/DrawResult';
+import React from 'react';
 import MatchProcess from '../pixi/Match/MatchProcess';
 import MatchComplete from '../pixi/Match/MatchComplete';
-
-
+import BorderTemplate from '../template/BorderTemplate';
 
 const MyRoutes = () => {
     return (
         <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
             <Route element={<GameTemplate />}>
                 <Route path="/test" element={<BattlePage />} />
                 <Route path="/match" element={<Match />} />
                 <Route path="/draw" element={<DrawPage />} />
-                <Route path="/draw/loading" element={<DrawOneLoading />} />
                 <Route path="/draw/result" element={<DrawResult />} />
             </Route>
             <Route element={<BasicTemplate />}>
@@ -36,11 +32,10 @@ const MyRoutes = () => {
                 <Route path="/mix" element={<Mix />}></Route>
                 <Route path="/match2" element={<MatchProcess />}></Route>
                 <Route path="/match3" element={<MatchComplete />}></Route>
-
             </Route>
-            <Route element={<BasicTemplate />}>
-                <Route path="/market" element={<MarketPage />}></Route>
-                <Route path="/rank" element={<RankingPage />}></Route>
+            <Route element={<BorderTemplate />}>
+                <Route path="/login" element={<LoginPage />}></Route>
+                <Route path="/singup" element={<SignupPage />}></Route>
             </Route>
         </Routes>
     );
