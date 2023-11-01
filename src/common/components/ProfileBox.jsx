6a4afaf140202291c90410312cd256f0cb86565a
JUNGI JEONG <junejjk@gmail.com>
2023-11-01 15:12:45 +0900

    Firebase setting

--- src/common/components/ProfileBox.jsx
+++ src/common/components/ProfileBox.jsx
@@ -1,22 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import { api } from '../../network/api';
 import './ProfileBox.css';
-import attackIcon from '../imgs/AttackPower.png';
-import defenseIcon from '../imgs/DefensePower.png';
-import lifeIcon from '../imgs/Life.png';
-import rateIcon from '../imgs/Rate.png';
-import dogIcon from '../imgs/Dog.png';
-import catIcon from '../imgs/Cat.png';
-import gliresIcon from '../imgs/Glires.png';
-import fishIcon from '../imgs/Fish.png';
-import birdIcon from '../imgs/Bird.png';
-import defaultIcon from '../imgs/Default.png';
-import landIcon from '../imgs/Land.png';
-import seaIcon from '../imgs/Sea.png';
-import mountainIcon from '../imgs/Mountain.png';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 
 const ProfileBox = () => {
     const [profile, setProfile] = useState({});
+    const [icons, setIcons] = useState({});
+
+    const fetchImageFromFirebase = async (imageName) => {
+        const storage = getStorage();
+        const imageRef = ref(storage, imageName);
+
+        try {
+            return await getDownloadURL(imageRef);
+        } catch (error) {
+            console.error(`Failed to fetch ${imageName} URL:`, error);
+            return null;
+        }
+    };
+
+    const loadIcons = async () => {
+        const imageNames = [
+            'AttackPower.png',
+            'DefensePower.png',
+            'Life.png',
+            'Rate.png',
+            'Dog.png',
+            'Cat.png',
+            'Glires.png',
+            'Fish.png',
+            'Bird.png',
+            'Default.png',
+            'Land.png',
+            'Sea.png',
+            'Mountain.png',
+        ];
+
+        const newIcons = {};
+        for (let name of imageNames) {
+            newIcons[name.split('.')[0]] = await fetchImageFromFirebase(name);
+        }
+
+        setIcons(newIcons);
+    };
 
     const getProfileData = async () => {
         try {
@@ -32,58 +58,59 @@ const ProfileBox = () => {
     const getSpeciesIcon = (species) => {
         switch (species) {
             case 'DOG':
-                return { icon: dogIcon, text: '개' };
+                return { icon: 'Dog', text: '개' };
             case 'CAT':
-                return { icon: catIcon, text: '고양이' };
+                return { icon: 'Cat', text: '고양이' };
             case 'GLIRES':
-                return { icon: gliresIcon, text: '설치동물' };
+                return { icon: 'Glires', text: '설치동물' };
             case 'FISH':
-                return { icon: fishIcon, text: '어류' };
+                return { icon: 'Fish', text: '어류' };
             case 'BIRD':
-                return { icon: birdIcon, text: '조류' };
+                return { icon: 'Bird', text: '조류' };
             default:
-                return { icon: defaultIcon, text: '알 수 없음' };
+                return { icon: 'Default', text: '알 수 없음' };
         }
     };
 
     const getLandFormIcon = (landForm) => {
         switch (landForm) {
             case 'LAND':
-                return { icon: landIcon, text: '육지' };
+                return { icon: 'Land', text: '육지' };
             case 'SEA':
-                return { icon: seaIcon, text: '바다' };
+                return { icon: 'Sea', text: '바다' };
             case 'MOUNTAIN':
-                return { icon: mountainIcon, text: '산' };
+                return { icon: 'Mountain', text: '산' };
             default:
-                return { icon: defaultIcon, text: '알 수 없음' };
+                return { icon: 'Default', text: '알 수 없음' };
         }
     };
 
     useEffect(() => {
         getProfileData();
+        loadIcons();
     }, []);
 
     return (
         <div className="profile-box">
             <div className="profile-item">
-                <img src={getSpeciesIcon(profile.species).icon} alt="species" />
+                <img src={icons[getSpeciesIcon(profile.species).icon]} alt="species" />
                 <span>{getSpeciesIcon(profile.species).text}</span>
 
-                <img src={getLandFormIcon(profile.landForm).icon} alt="landForm" />
+                <img src={icons[getLandFormIcon(profile.landForm).icon]} alt="landForm" />
                 <span>{getLandFormIcon(profile.landForm).text}</span>
             </div>
             <div className="stats-item">
-                <img src={attackIcon} alt="attackPower" />
+                <img src={icons.AttackPower} alt="attackPower" />
                 <span>{profile.attackPower}</span>
 
-                <img src={defenseIcon} alt="defensePower" />
+                <img src={icons.DefensePower} alt="defensePower" />
                 <span>{profile.defensePower}</span>
 
-                <img src={lifeIcon} alt="Life" />
+                <img src={icons.Life} alt="Life" />
                 <span>{profile.life}</span>
             </div>
             <div className="rate-item">
-                <img src={rateIcon} alt="ratePerHour" />
+                <img src={icons.Rate} alt="ratePerHour" />
                 <span>시간당 자원생산량</span>
                 <span>식량: {profile.food}</span>
                 <span>철: {profile.iron}</span>
