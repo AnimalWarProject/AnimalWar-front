a9b8e011ec1436fabc31e9b3fe597a600515753b
sein <lsi6601@gmail.com>
2023-11-14 15:23:23 +0900

    chtting messageRecord width chage

--- src/common/components/Chatting.jsx
+++ src/common/components/Chatting.jsx
@@ -25,7 +25,7 @@ const Chatting = () => {
         const accessToken = localStorage.getItem('accessToken');
 
         axios
-            .get('http://192.168.0.44:8000/api/v1/user', {
+            .get('http://localhost:8000/api/v1/user', {
                 headers: {
                     ContentType: 'application/json',
                     Authorization: `Bearer ${accessToken}`,
@@ -39,7 +39,7 @@ const Chatting = () => {
                 console.log(err);
             });
 
-        const socket = new SockJS('http://192.168.0.44:8082/stomp-endpoint'); // #1 socket열기(server와 연결) // npm install stompjs
+        const socket = new SockJS('http://localhost:8082/stomp-endpoint'); // #1 socket열기(server와 연결) // npm install stompjs
         const stomp = Stomp.over(socket); // STOMP 클라이언트를 통해 STOMP 프로토콜을 사용하여 메시지를 전송 및 수신
 
         stomp.connect({}, (frame) => {
@@ -130,12 +130,12 @@ const Chatting = () => {
                     {greetings.map((item, idx) => (
                         <div key={idx} className={classes.message_container}>
                             <img
-                                style={{ width: '50px', height: '50px' }}
+                                style={{  width: '50px', height: '50px', objectFit: 'cover'}}
                                 className={classes.profileImg}
                                 src={profileImage}
                             />
                             <div className={classes.profileNickname}>{item.nickname}</div>
-                            <div className={classes.messageRecord}>{item.message}</div>
+                            <div className={classes.messageRecord} style={{width : '24rem'}}>{item.message}</div>
                         </div>
                     ))}
                 </div>
