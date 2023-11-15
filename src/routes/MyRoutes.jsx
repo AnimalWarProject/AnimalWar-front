581cf3cff871dc21fc114848e15c66cfb58a72bd
ptwoj <kauze1998@gamil.com>
2023-11-15 17:39:20 +0900

    merge time

--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -22,7 +22,8 @@ import UpGradeResult from '../pixi/upGrade/components/UpGradeResult';
 import UserInfo from '../pages/userInfo/UserInfo';
 import TerrainPage from '../pixi/terrain/TerrainPage';
 import Exchange from '../pixi/Exchange/Exchange';
-import Skill from "../pixi/battle/components/Skill";
+import AttackerSkill from "../pixi/battle/components/AttackerSkill";
+import DefenderSkill from "../pixi/battle/components/DefenderSkill";
 import Battle from "../pixi/battle/components/Battle";
 import GameResult from "../pixi/battle/components/GameResult";
 import Place from '../pixi/place/Place';
@@ -52,9 +53,10 @@ const MyRoutes = () => {
                 <Route path="/match" element={<Match />} />
                 <Route path="/match2" element={<MatchProcess />} />
                 <Route path="/match3" element={<MatchComplete />} />
-                <Route path="/battle" element={<Skill />}></Route>
-                <Route path="/battle1" element={<Battle />}></Route>
-                <Route path="/battle2" element={<GameResult />}></Route>
+                <Route path="/battle" element={<AttackerSkill />}></Route>
+                <Route path="/battle1" element={<DefenderSkill />}></Route>
+                <Route path="/battle2" element={<Battle />}></Route>
+                <Route path="/battle3" element={<GameResult />}></Route>
                 <Route path="/exchange" element={<Exchange />}></Route>
                 <Route path="/place" element={<Place />}></Route>
             </Route>
