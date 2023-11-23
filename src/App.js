50613a50225f55b146152002bc157d250bd028fa
unknown <you@example.com>
2023-11-23 14:23:52 +0900

    d

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MyRoutes from './routes/MyRoutes';
 import { FirebaseConfig } from './network/FirebaseConfig.jsx';
 import { initializeApp } from 'firebase/app';
 
-initializeApp(FirebaseConfig);
+
 
 function App() {
     return <MyRoutes />;
